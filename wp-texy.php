1b17dfad3948579e22e4bfa83403fe709b7e8174
Matěj Humpál <finwe@finwe.info>
2022-02-08 14:48:50 +0100

    Initial version

---
+++ wp-texy.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+Plugin Name: WP Texy
+Version: 0.1
+Description: Texy! is text-to-HTML formatter and converter library. It allows you to write structured documents without knowledge or using of HTML language. You write documents in humane easy-to-read plain text format and Texy! converts it to structurally and valid (X)HTML code.
+Author: Finwe, David Grudl
+*/
+
+class TexyPlugin
+{
+
+	private static $texy;
+
+	public static function texy($content)
+	{
+		self::init();
+
+		return self::$texy->process($content);
+	}
+	
+	public static function init()
+	{
+		if (!self::$texy) {
+			self::$texy = new \Texy\Texy();
+			self::$texy->headingModule->top = 3;
+			self::$texy->allowed['heading/underlined'] = true;
+			self::$texy->allowed['heading/surrounded'] = true;
+		}
+	}
+
+}
+
+
+
+add_filter('the_content', [TexyPlugin::class, 'texy']);
